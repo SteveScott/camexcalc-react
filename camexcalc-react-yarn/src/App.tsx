0154beda43aa56c801f6eb81fd7f30261d9785a9
Steve Scott <stevescott517@gmail.com>
2024-12-21 21:15:57 -0500

    add material ui in attempt to implement non-linear sliders. Not working yet.

--- camexcalc-react-yarn/src/App.tsx
+++ camexcalc-react-yarn/src/App.tsx
@@ -1,10 +1,57 @@
-import React {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactSlider from 'react-slider';
+import Slider from '@mui/material/Slider';
+import TextField from '@mui/material/TextField';
+import { Input } from '@mui/material';
+
+function scale (x: number) {
+  return 2 ** x;
+}
 
 function App() {
-  const [fStop, setFStop] = useState(4);
+  const isoValues = [3, 7, 12, 25, 50, 100, 200, 400, 800, 1600, 3200];
+  const isoMarks = isoValues.map((value) => ({value, label: value.toString()}));
+  const fstopVlaues = [ 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64];
+  const fstopMarks = fstopVlaues.map((value) => ({value, label:value.toString()}));
+  const shutterSpeedValues = [1/200, 1/1000, 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/8, 1/4, 1/2, 1, 2, 4, 8, 15, 30];
+  const shutterSpeedMarks = [
+    {value: 1/200, label: '1/200'},
+    {value: 1/1000, label: '1/1000'},
+    {value: 1/500, label: '1/500'},
+    {value: 1/250, label: '1/250'},
+    {value: 1/125, label: '1/125'},
+    {value: 1/60, label: '1/60'},
+    {value: 1/30, label: '1/30'},
+    {value: 1/15, label: '1/15'},
+    {value: 1/8, label: '1/8'},
+    {value: 1/4, label: '1/4'},
+    {value: 1/2, label: '1/2'},
+    {value: 1, label: '1'},
+    {value: 2, label: '2'},
+    {value: 4, label: '4'},
+    {value: 8, label: '8'},
+    {value: 15, label: '15'},
+    {value: 30, label: '30'},
+  ]
+
+  const [fStop, setFStop] = useState(16);
+  const [iso, setIso] = useState(100);
+  const [shutterSpeed, setShutterSpeed] = useState(1/125);
+  const [gn, setGn] = useState(0);
+
+  const handleFStopChange = (event: Event, newValue: number | number[]) => {
+    setFStop(newValue as number);
+  };
+  const handleIsoChange = (event: Event, newValue: number | number[]) => {
+    setIso(newValue as number);
+  };
+  const handleShutterSpeedChange = (event: Event, newValue: number | number[]) => {
+    setShutterSpeed(newValue as number);
+  };
+  const handleGnChange = (event: Event, newValue: number | number[]) => {
+    setGn(newValue as number);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -29,35 +76,40 @@ function App() {
                 <label id="FStopDisplay">F{fStop}</label>
               </div>
               <div className="col-xs-8 col-sm-8 col-md-6">
-                <ReactSlider
+                <Slider
                   className="slider"
-                  thumbClassName="thumb"
-                  trackClassName="track"
-                  min={0}
-                  max={12}
-                  step={1}
+                  id="FStop"
+                  min={fstopVlaues[0]}
+                  max={fstopVlaues[fstopVlaues.length - 1]}
+                  step={null}
                   value={fStop}
-                  onChange={(value) => setFStop(value)}
+                  onChange={handleFStopChange}
+                  scale = {scale}
+                  marks={fstopMarks}
+                  aria-labelledby='non-linear-slider'
+                  valueLabelDisplay='off'
+                  //onChange={(value: Number) => setFStop(value)}
                 />
               </div>
             </div>
             <div className="row">
               <div className="col-xs-3 col-sm-2 col-md-4">Shutter Speed</div>
               <div className="hidden-xs col-sm-2 col-md-2">
-                <label id="ShutterSpeedDisplay">1:125</label>
+                <label id="ShutterSpeedDisplay">{shutterSpeed}</label>
               </div>
               <div className="col-xs-8 col-sm-8 col-md-6">
-                <input
-                  id="ShutterSpeed"
-                  data-slider-id="ShutterSpeed"
-                  asp-for="ShutterSpeed"
-                  type="text"
-                  data-slider-handle="custom"
-                  data-slider-min="0"
-                  data-slider-max="20"
-                  data-slider-step="1"
-                  data-slider-value="0"
+                <Slider
                   className="slider"
+                  id="ShutterSpeed"
+                  min={shutterSpeedValues[0]}
+                  max={shutterSpeedValues[shutterSpeedValues.length - 1]}
+                  step={null}
+                  value={shutterSpeed}
+                  scale = {scale}
+                  onChange={handleShutterSpeedChange}
+                  valueLabelDisplay='off'
+                  marks={shutterSpeedMarks}
+                  aria-labelledby='non-linear-slider'
                 />
               </div>
             </div>
@@ -65,20 +117,22 @@ function App() {
           <div className="row">
               <div className="col-xs-3 col-sm-2 col-md-4">ISO</div>
               <div className="hidden-xs col-sm-2 col-md-2">
-                <label id="IsoDisplay">100</label>
+                <label id="IsoDisplay">{iso}</label>
               </div>
               <div className="col-xs-8 col-sm-8 col-md-6">
-                <input
-                  id="Iso"
-                  data-slider-id="Iso"
-                  asp-for="Iso"
-                  type="text"
-                  data-slider-handle="custom"
-                  data-slider-min="0"
-                  data-slider-max="7"
-                  data-slider-step="1"
-                  data-slider-value="2"
+                <Slider
                   className="slider"
+                  id="Iso"
+                  min = {isoValues[0]}
+                  max = {isoValues[isoValues.length - 1]}
+                  step={null}
+                  value={iso}
+                  scale = {scale}
+                  onChange={handleIsoChange}
+                  valueLabelDisplay='off'
+                  marks={isoMarks}
+                  aria-labelledby="non-linear-slider"
+
                 />
               </div>
             </div>
@@ -87,11 +141,12 @@ function App() {
             <div className="row">
               <div className="col-xs-4">Flash Guide Number (&#64 ISO 100)</div>
               <div className="col-xs-2 ">
-                <input
+                <TextField
                   className="NumberDisplay"
                   id="GN"
                   value="0"
                   type="number"
+                  onChange={handleGnChange}
                 />
               </div>
               <div className="col-xs-3">
@@ -113,14 +168,9 @@ function App() {
               </div>
             </div>
           </div>
-
-
-
       </header>
     </div>
-    
-
   );
-}
+};
 
 export default App;
