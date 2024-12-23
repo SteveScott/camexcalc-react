fc03e0afd66a52c14daacc4b45293e97eb384b1e
Steve Scott <stevescott517@gmail.com>
2024-12-22 19:50:45 -0500

    shift to ui.shadcn .completely broken.

--- camexcalc-react-yarn/src/App.tsx
+++ camexcalc-react-yarn/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Slider from '@mui/material/Slider';
+import logo from 'logo.svg';
+import 'App.css';
+import { Label } from  '@/components/ui/label';
+import { Slider } from './components/ui/slider';
 import TextField from '@mui/material/TextField';
 import { Input } from '@mui/material';
 
@@ -12,6 +13,7 @@ function scale (x: number) {
 function App() {
   const isoValues = [3, 7, 12, 25, 50, 100, 200, 400, 800, 1600, 3200];
   const isoMarks = isoValues.map((value) => ({value, label: value.toString()}));
+
   const fstopVlaues = [ 1, 1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32, 45, 64];
   const fstopMarks = fstopVlaues.map((value) => ({value, label:value.toString()}));
   const shutterSpeedValues = [1/200, 1/1000, 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/8, 1/4, 1/2, 1, 2, 4, 8, 15, 30];
@@ -36,22 +38,29 @@ function App() {
   ]
 
   const [fStop, setFStop] = useState(16);
-  const [iso, setIso] = useState(100);
+  const [isoIndex, setIso] = useState(isoValues.indexOf(100));
   const [shutterSpeed, setShutterSpeed] = useState(1/125);
   const [gn, setGn] = useState(0);
+
+  const currentISOValue = isoValues[isoIndex]
+
+
+  const handleIsoChange = (newValue) => {
+    setIso(newValue[0]);
+  }
 
   const handleFStopChange = (event: Event, newValue: number | number[]) => {
     setFStop(newValue as number);
   };
-  const handleIsoChange = (event: Event, newValue: number | number[]) => {
-    setIso(newValue as number);
-  };
+
   const handleShutterSpeedChange = (event: Event, newValue: number | number[]) => {
     setShutterSpeed(newValue as number);
   };
+
   const handleGnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setGn(Number(event.target.value));
   };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -76,20 +85,7 @@ function App() {
                 <label id="FStopDisplay">F{fStop}</label>
               </div>
               <div className="col-xs-8 col-sm-8 col-md-6">
-                <Slider
-                  className="slider"
-                  id="FStop"
-                  min={fstopVlaues[0]}
-                  max={fstopVlaues[fstopVlaues.length - 1]}
-                  step={null}
-                  value={fStop}
-                  onChange={handleFStopChange}
-                  scale = {scale}
-                  marks={fstopMarks}
-                  aria-labelledby='non-linear-slider'
-                  valueLabelDisplay='off'
-                  //onChange={(value: Number) => setFStop(value)}
-                />
+
               </div>
             </div>
             <div className="row">
@@ -97,43 +93,37 @@ function App() {
               <div className="hidden-xs col-sm-2 col-md-2">
                 <label id="ShutterSpeedDisplay">{shutterSpeed}</label>
               </div>
-              <div className="col-xs-8 col-sm-8 col-md-6">
-                <Slider
-                  className="slider"
-                  id="ShutterSpeed"
-                  min={shutterSpeedValues[0]}
-                  max={shutterSpeedValues[shutterSpeedValues.length - 1]}
-                  step={null}
-                  value={shutterSpeed}
-                  scale = {scale}
-                  onChange={handleShutterSpeedChange}
-                  valueLabelDisplay='off'
-                  marks={shutterSpeedMarks}
-                  aria-labelledby='non-linear-slider'
-                />
-              </div>
+
             </div>
           </div>
           <div className="row">
               <div className="col-xs-3 col-sm-2 col-md-4">ISO</div>
-              <div className="hidden-xs col-sm-2 col-md-2">
-                <label id="IsoDisplay">{iso}</label>
-              </div>
+              <div className="hidden-xs col-sm-2 col-md-2"></div>
               <div className="col-xs-8 col-sm-8 col-md-6">
                 <Slider
-                  className="slider"
-                  id="Iso"
-                  min = {isoValues[0]}
-                  max = {isoValues[isoValues.length - 1]}
-                  step={null}
-                  value={iso}
-                  scale = {scale}
+                  className="w-full"
+                  id="iso-slider"
+                  min = {0}
+                  max = {isoValues.length - 1}
+                  step={1}
+                  value={[isoIndex]}
                   onChange={handleIsoChange}
-                  valueLabelDisplay='off'
-                  marks={isoMarks}
-                  aria-labelledby="non-linear-slider"
-
                 />
+                <div className="flex justify-between text-xs text-gray-500">
+                  {isoValues.map((value, index) => (
+                    <span 
+                    key={value}
+                    className="relative"
+                    style={{
+                      position: 'absolute',
+                      left: `${index * 100 / (isoValues.length - 1)}%`,
+                      transform: 'translateX(-50%)'
+                    }}
+                    >
+                      {value}  
+                    </span>
+                  ))
+                  }
               </div>
             </div>
           </div>
@@ -168,6 +158,7 @@ function App() {
               </div>
             </div>
           </div>
+        </div>
       </header>
     </div>
   );
